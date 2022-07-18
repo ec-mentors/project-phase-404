b283e6aafa1573145f87d81d11370ed8d639fbed
stuepfnick <stuepfnick@gmx.at>
2022-07-19 01:19:06 +0200

    added filter for name or abbreviation

--- crypto/cryptolog/src/main/java/io/everyonecodes/cryptolog/controller/SiteController.java
+++ crypto/cryptolog/src/main/java/io/everyonecodes/cryptolog/controller/SiteController.java
@@ -5,13 +5,17 @@ import io.everyonecodes.cryptolog.data.Coin;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestClientException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class SiteController {
 
+    private List<Coin> coinList;
     private final CoingeckoClient client;
 
     public SiteController(CoingeckoClient client) {
@@ -36,12 +40,32 @@ public class SiteController {
     @GetMapping("/home")
     public String top100(Model model) {
         try {
-            List<Coin> coinList = client.getTop100ByMarketCap();
+            coinList = client.getTop100ByMarketCap();
             model.addAttribute(coinList);
             return "home";
         } catch (RestClientException e) {
             model.addAttribute("errorMessage", e.getMessage());
-            return "clientError.html";
+            return "clientError";
         }
     }
+
+    @PostMapping("/home")
+    public String filter(Model model, @RequestParam("filter") String filter) {
+        if (coinList == null) {
+            try {
+                coinList = client.getTop100ByMarketCap();
+            } catch (RestClientException e) {
+                model.addAttribute("errorMessage", e.getMessage());
+                return "clientError";
+            }
+        }
+        String filterString = filter.toLowerCase();
+        var filteredList = coinList.stream().filter(coin ->
+                coin.getName().toLowerCase().contains(filterString) ||
+                        coin.getSymbol().toLowerCase().contains(filterString)
+        ).toList();
+        model.addAttribute("coinList", filteredList);
+        return "home";
+    }
+
 }
