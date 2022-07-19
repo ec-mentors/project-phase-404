89776120b6c2b40607db5f84c38d5977a49838b7
Stuepfnick <stuepfnick@MacBook-Pro-von-Stuepfnick.local>
2022-07-19 22:20:11 +0200

    forgot to strip js/main.js

--- crypto/cryptolog/src/main/resources/static/js/main.js
+++ crypto/cryptolog/src/main/resources/static/js/main.js
@@ -1,10 +1,5 @@
 function setMenuEntryActive() {
-    // find the link matching browser-url
-    //const targetLink = $("a[href='" + window.location.pathname + "']");
     const targetLink = document.querySelector("a[href='" + window.location.pathname + "']");
-    // set targetLink active
-    //$(".nav li a").removeClass("active");
-    //targetLink.addClass("active");
     targetLink.classList.add("active");
 }
 
