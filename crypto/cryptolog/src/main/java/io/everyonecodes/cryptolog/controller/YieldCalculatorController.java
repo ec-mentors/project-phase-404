88c5026bc3edfa5527e448979d31e9a58e68c0fa
RaulBodog <raulbodog993@gmail.com>
2022-08-01 15:34:36 +0200

    Included Moving Averages

--- crypto/cryptolog/src/main/java/io/everyonecodes/cryptolog/controller/YieldCalculatorController.java
+++ crypto/cryptolog/src/main/java/io/everyonecodes/cryptolog/controller/YieldCalculatorController.java
@@ -30,14 +30,16 @@ public class YieldCalculatorController {
         this.userService = userService;
         this.client = client;
     }
+
     private String test;
+
     @GetMapping("/calculator")
     public String getYieldResults(Model model,
                                   @RequestParam(required = false) String monthlyAmount,
                                   @RequestParam(required = false) String period,
                                   @RequestParam(required = false) String days,
                                   Principal principal) {
-        if (monthlyAmount == null || period == null || days == null){
+        if (monthlyAmount == null || period == null || days == null) {
             return "calculator";
         }
 
@@ -45,15 +47,22 @@ public class YieldCalculatorController {
         User user = userService.loadLoggedInUser(principal);
 
 
-        if(!user.getAssetsAllocation().equals("none")) {
-
-            if (days == null || days.isBlank()) {
-                days = "0";
-            }
+        if (!user.getAssetsAllocation().equals("none")) {
             if (!days.equals("0")) {
+                if (monthlyAmount.isBlank()) {
+                    monthlyAmount = "0";
+                }
+                if (period.isBlank()) {
+                    period = "0";
+                }
 
-                //System.out.println("days 0");
-                return "calculatorlock";
+                yieldCalculatorService.setAttributesWithMovingAverage(period, monthlyAmount,
+                        model, principal, Integer.parseInt(days));
+
+                model.addAttribute("days", days);
+
+                return "calculator";
+
 
             } else {
                 if (monthlyAmount.isBlank()) {
@@ -64,33 +73,10 @@ public class YieldCalculatorController {
                 }
                 yieldCalculatorService.checkParameters(monthlyAmount, period);
                 yieldCalculatorService.setAttributes(period, monthlyAmount, model, principal);
-                model.addAttribute("day", days);
-             //   System.out.println(days);
-//            System.out.println(user.getDay());
-//            System.out.println(day.getDay());
                 return "calculator";
             }
         }
 
         return "calculatorlock";
     }
-//    @PostMapping(value = "/calculator")
-//    public String updateDay(Principal principal, @ModelAttribute("day")
-//            Day day , Model model,BindingResult bindingResult) {
-//        User user = userService.loadLoggedInUser(principal);
-//        user.setDay(day.getDay());
-//        userServiceImp.save(user);
-//        model.addAttribute("assetMessage", "Missing coin from Tier 2 or 3. Please mind that you need to continue adding coins to your portfolio in order to select this particular asset allocation");
-//        System.out.println("post " + day.getDay());
-//        System.out.println("post " + user.getDay());
-//        if (bindingResult.hasErrors()){
-//            System.out.println("error");
-//        }
-//
-//    return "calculator";
-//
-//    }
-
-
-
 }
